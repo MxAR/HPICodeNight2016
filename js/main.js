50a762d26aeb90060661d085de61b6d28ae5d423
Marc <momueller@me.com>
2016-11-07 21:38:55 +0100

    Responsive and extras commit
    > Site is now responsive for all devices including iPhone5
    > Added CYMK conversion / support
    > Restyled Hex Input and Button
    > Tiny Change to footer

--- js/main.js
+++ js/main.js
@@ -125,6 +125,47 @@ function hsvCut (hsv) {
 	return hsv + '%';
 }
 
+function hexToCMYK (hex) {
+	var computedC = 0;
+	var computedM = 0;
+	var computedY = 0;
+	var computedK = 0;
+
+	hex = (hex.charAt(0)=="#") ? hex.substring(1,7) : hex;
+
+	if (hex.length != 6) {
+	alert ('Invalid length of the input hex value!');   
+	return; 
+	}
+	if (/[0-9a-f]{6}/i.test(hex) != true) {
+	alert ('Invalid digits in the input hex value!');
+	return; 
+	}
+
+	var r = parseInt(hex.substring(0,2),16); 
+	var g = parseInt(hex.substring(2,4),16); 
+	var b = parseInt(hex.substring(4,6),16); 
+
+	// BLACK
+	if (r==0 && g==0 && b==0) {
+	computedK = 1;
+	return [0,0,0,1];
+	}
+
+	computedC = 1 - (r/255);
+	computedM = 1 - (g/255);
+	computedY = 1 - (b/255);
+
+	var minCMY = Math.min(computedC,Math.min(computedM,computedY));
+
+	computedC = (computedC - minCMY) / (1 - minCMY) ;
+	computedM = (computedM - minCMY) / (1 - minCMY) ;
+	computedY = (computedY - minCMY) / (1 - minCMY) ;
+	computedK = minCMY;
+
+	return [computedC,computedM,computedY,computedK];
+}
+
 var colorOne = '#FFFFFF';
 var colorTwo = '#000000';
 
@@ -153,7 +194,7 @@ function hexSearch() {
 	}
 
 	// var color0 = '#7822c8';
-	// var color1 = '#fafffd';
+	// var color1 = '#99fd59';
 
 	// colorOne = color0;
 	// colorTwo = color1;
@@ -172,7 +213,10 @@ function hexSearch() {
 	// 	// HSV Output
 	// 	var hsv = rgbToHsv(rgb[0],rgb[1],rgb[2]);
 	// 	$('#hsv' + i).html('hsv(' + Math.round(hsv[0]) + ', ' + hsvCut(hsv[1]) + ', ' + hsvCut(hsv[2]) + ')');
-		
+	// 	// CYMK Output
+	// 	var cymk = hexToCMYK(color);
+	// 	$('#cymk' + i).html('cymk(' + cymk[0].toFixed(4) + ', ' + cymk[1].toFixed(4) + ', ' + cymk[2].toFixed(4) + ', ' + cymk[3].toFixed(4) + ')');
+
 	// 	// Tone For Loop
 	// 	for (var j = 0; j < 21; j++) {
 	// 		var shade = (lum(color, ((j * -0.1) + 1))).toUpperCase();
@@ -209,7 +253,10 @@ function hexSearch() {
 				// HSV Output
 				var hsv = rgbToHsv(rgb[0],rgb[1],rgb[2]);
 				$('#hsv' + i).html('hsv(' + Math.round(hsv[0]) + ', ' + hsvCut(hsv[1]) + ', ' + hsvCut(hsv[2]) + ')');
-				
+				// CYMK Output
+				var cymk = hexToCMYK(color);
+				$('#cymk' + i).html('cymk(' + cymk[0].toFixed(4) + ', ' + cymk[1].toFixed(4) + ', ' + cymk[2].toFixed(4) + ', ' + cymk[3].toFixed(4) + ')');
+
 				// Tone For Loop
 				for (var j = 0; j < 21; j++) {
 					var shade = (lum(color, ((j * -0.1) + 1))).toUpperCase();
