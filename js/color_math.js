12d48f85ef701e6a8157860de50180a0a37003a4
mxar <max.rosssner@gmail.com>
2016-12-24 10:39:33 +0100

    normalization of the color vectors

--- js/color_math.js
+++ js/color_math.js
@@ -11,22 +11,22 @@ const COEF = [
 
 function GetCombo(V, P = 255.0) {
     var ROMA = math.zeros(3, 3);
-    var Axis = OV = [ 0, 0, 0 ];
+    var Axis = OV = [0, 0, 0];
     var Angle = s = 0;
     var FT = true;
 
-    var W = V.map(function(x) { return Math.abs(x); });                         // 
+    var W = V.map(function (x) { return Math.abs(x); });                        // 
     if (Math.max(W) == 0) {                                                     // handling of 
-        W = W.map(function(x) { return (Math.random() * 255) - 127.5; });       // black color codes
-        s = Math.sqrt((W.reduce(function(p, q) { return p + q; }, 0)) / 10);    //
-        V = W.map(function(x) { return x / s; });                               //
+        W = W.map(function (x) { return (Math.random() * 255) - 127.5; });      // black color codes
+        s = Math.sqrt((W.reduce(function (p, q) { return p + q; }, 0)) / 10);   //
+        V = W.map(function (x) { return x / s; });                              //
     }                                                                           //
 
     var Lum = L2Norm(V) / MLUM;                                                                                                                             // calculation of 
     var RefAngle = (COEF[0] * Math.pow((Lum - COEF[1]), 4)) + (COEF[2] * Math.pow(Lum, 3)) + (COEF[3] * Math.pow(Lum, 2)) + (COEF[4] * (Lum)) + (COEF[5]);  // the angle 
 
     do {
-        Angle = RefAngle + Math.exp(Math.pow(Math.random()-0.5, 2) * -7.5) * P * SIG;
+        Angle = RefAngle + Math.exp(Math.pow(Math.random() - 0.5, 2) * -7.5) * P * SIG;
         if (FT) { Axis = GetOrthogonalUnitVector(V); FT = false } else {
             var theta = Math.random() * 90;                                 //
             var psi = Math.random() * 90;                                   //
@@ -50,39 +50,33 @@ function GetCombo(V, P = 255.0) {
 
         OV = math.multiply(V, ROMA)["_data"];
         s = OV[0] < 0 && OV[1] < 0 && OV[2] < 0 ? -1 : 1;
-        OV = OV.map(function(x) { return x * s; });
+        OV = OV.map(function (x) { return x * s; });
 
         OV = NormalizeBrightness(OV);
-        if (Math.max(OV) > 255.0) { s = 255 / Math.max(OV);  OV = OV.map(function(x) { return x * s; }); }
-        OV = OV.map(function(x) { return Math.round(Math.max(0, Math.min(x, 255))); });
+        s = (L2Norm(OV) / MLUM) * Math.exp(Math.pow(Math.random() - 0, 5, 2) * 1.1);
+        OV = OV.map(function (x) { return Math.round(Math.max(0, x / s)); });
 
-        console.log(V, OV, Axis, ROMA["_data"]);
-    } while((DotProduct(V, OV) / (L2Norm(V) * L2Norm(OV))) > 0.7);
+        console.log(V, OV, s, Axis, ROMA["_data"]);
+    } while ((DotProduct(V, OV) / (L2Norm(V) * L2Norm(OV))) > 0.7);
     return OV;
 
 }
 
 function RandomRGB() {
-    return ([0, 0, 0]).map(function(x) { return Math.round(Math.random() * 255.0);})
+    return ([0, 0, 0]).map(function (x) { return Math.round(Math.random() * 255.0); })
 }
 
-function NormalizeBrightness(V) {
-    var s = L2Norm(V) * Math.pow(MLUM * Math.exp(Math.pow(Math.random()-0.5, 2) / -0.9), -1);
-    console.log(s);
-    return V.map(function(x) { return V / s; });
-}
-
-function GetOrthogonalUnitVector(V) { 
-    var W = [ Math.random() * 10, Math.random() * -10, 0 ];
+function GetOrthogonalUnitVector(V) {
+    var W = [Math.random() * 10, Math.random() * -10, 0];
     W[2] = (V[0] * W[0] + V[1] * W[1]) / (-1 * (V[2] == 0 ? 1 : V[2]));
     var size = L2Norm(W);
-    return W.map(function(x) { return x / size; });
+    return W.map(function (x) { return x / size; });
 }
 
-function L2Norm(V) { 
-    return Math.sqrt(Math.pow(V[0], 2) + Math.pow(V[1], 2) + Math.pow(V[2], 2)); 
+function L2Norm(V) {
+    return Math.sqrt(Math.pow(V[0], 2) + Math.pow(V[1], 2) + Math.pow(V[2], 2));
 }
 
 function DotProduct(P, Q) {
-    return P[0]*Q[0] + P[1]*Q[1] + P[2]*Q[2];
+    return P[0] * Q[0] + P[1] * Q[1] + P[2] * Q[2];
 }
