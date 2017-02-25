3955099729b7538275ac0abed7b2228c338d9c2f
mxar <max.rosssner@gmail.com>
2017-02-25 18:00:41 +0100

    große verbesserung

--- js/color_math.js
+++ js/color_math.js
@@ -1,79 +1,68 @@
-const SIG = 15.903165825358679 / 255.0;
 const MLUM = L2Norm([255, 255, 255]);
-const COEF = [
-    108.29450583260494,
-    0.9192165698606374,
-    -18.95601236539241,
-    -116.97676027964721,
-    150.03360140094875,
-    -19.080783430139363
-];
 
-function GetCombo(V, P = 255.0) {
-    var Lum = L2Norm(V) / MLUM;  
-    var Axis, OV, OVX, OVY, OVZ, Angle, s;                                                                                                                  
-    var RefAngle = (COEF[0] * Math.pow((Lum - COEF[1]), 4)) + (COEF[2] * Math.pow(Lum, 3)) + (COEF[3] * Math.pow(Lum, 2)) + (COEF[4] * (Lum)) + (COEF[5]);
+function GetCombo(V) {
+    var Axis, OV, OVX, OVY, OVZ, Angle, s
+    var lum = L2Norm(V) / MLUM                                                                                                                  
  
-    if (Math.max.apply(null, V) == 0) {                                         //
-        V = RandomRGB();                                                        // 
-        s = Math.sqrt((V.reduce(function (p, q) { return p + q; }, 0)) / 10);   // handling of
-        V = V.map(function (x) { return x / s; });                              // black colour codes
-    }                                                                           //
+    if (Math.max.apply(0, V) == 0) { V = RandomRGB() }                                                                          
 
-    do {
-        Angle = RefAngle + Math.exp(Math.pow(Math.random() - 0.5, 2) * -7.5) * P * SIG;
-        Axis = GetOrthogonalUnitVector(V);
+    Angle = 50 + (Math.random() * 10)
+    Axis = GetOrthogonalUnitVector(V)
 
-        OVX = [
-            Math.cos(Angle) + (Math.pow(Axis[0], 2) * (1 - Math.cos(Angle))),           //
-            (Axis[0] * Axis[1] * (1 - Math.cos(Angle))) - (Axis[2] * Math.sin(Angle)),  // X-Row
-            (Axis[0] * Axis[2] * (1 - Math.cos(Angle))) + (Axis[1] * Math.sin(Angle))   //
-        ];
+    s = L2Norm(V)
+    V = V.map(function (x) { return x / s })
 
-        OVY = [
-            (Axis[0] * Axis[1] * (1 - Math.cos(Angle))) + (Axis[2] * Math.sin(Angle)),  //
-            Math.cos(Angle) + (Math.pow(Axis[1], 2) * (1 - Math.cos(Angle))),           // Y-Row
-            (Axis[1] * Axis[2] * (1 - Math.cos(Angle))) - (Axis[0] * Math.sin(Angle))   //
-        ];
+    OVX = [
+        Math.cos(Angle) + (Math.pow(Axis[0], 2) * (1 - Math.cos(Angle))),           //
+        (Axis[0] * Axis[1] * (1 - Math.cos(Angle))) - (Axis[2] * Math.sin(Angle)),  // X-Row
+        (Axis[0] * Axis[2] * (1 - Math.cos(Angle))) + (Axis[1] * Math.sin(Angle))   //
+    ]
 
-        OVZ = [
-            (Axis[0] * Axis[2] * (1 - Math.cos(Angle))) - (Axis[1] * Math.sin(Angle)),  //
-            (Axis[1] * Axis[2] * (1 - Math.cos(Angle))) + (Axis[0] * Math.sin(Angle)),  // Z-Row
-            Math.cos(Angle) + (Math.pow(Axis[2], 2) * (1 - Math.cos(Angle)))            //
-        ];
+    OVY = [
+        (Axis[0] * Axis[1] * (1 - Math.cos(Angle))) + (Axis[2] * Math.sin(Angle)),  //
+        Math.cos(Angle) + (Math.pow(Axis[1], 2) * (1 - Math.cos(Angle))),           // Y-Row
+        (Axis[1] * Axis[2] * (1 - Math.cos(Angle))) - (Axis[0] * Math.sin(Angle))   //
+    ]
 
-        OV = [
-            DotProduct(OVX, V),
-            DotProduct(OVY, V),
-            DotProduct(OVZ, V)
-        ];
+    OVZ = [
+        (Axis[0] * Axis[2] * (1 - Math.cos(Angle))) - (Axis[1] * Math.sin(Angle)),  //
+        (Axis[1] * Axis[2] * (1 - Math.cos(Angle))) + (Axis[0] * Math.sin(Angle)),  // Z-Row
+        Math.cos(Angle) + (Math.pow(Axis[2], 2) * (1 - Math.cos(Angle)))            //
+    ]
 
-        println(OV)
+    OV = [
+        DotProduct(OVX, V),
+        DotProduct(OVY, V),
+        DotProduct(OVZ, V)
+    ]
 
-        s = OV[0] < 0 && OV[1] < 0 && OV[2] < 0 ? -1 : 1;
-        OV = OV.map(function (x) { return x * s; });
+    OV = OV.map(function (x) { return (x - 0.5) })
+    s = Math.max.apply(0, OV.map(Math.abs))
+    if (s > 0.5) { OV = OV.map(function (x) { return (x / s) * 0.5 }) }
+    OV = OV.map(function (x) { return x + 0.5 })
+    
+    s = L2Norm(OV)
+    OV = OV.map(function (x) { return x / s })
 
-        s = (MLUM / L2Norm(OV)) * Math.exp(Math.pow(Math.random() - 0, 5, 2) * 1.1);
-        OV = OV.map(function (x) { return Math.round(Math.max(0, x * s)); });;
-    } while ((DotProduct(V, OV) / (L2Norm(V) * L2Norm(OV))) > 0.7);
-    return OV;
+    s = Math.abs((1 - lum) + (lum <= 0.4 || lum >= 0.6 ? 0 : (Math.abs(Math.pow(2, -1 * Math.pow(Math.random(), 2)) - 0.5) * (lum >= 0.5 ? -1 : 1))))
+    return OV.map(function (x) { return Math.min(255, Math.round(x * s * MLUM)) })
 }
 
 function RandomRGB() {
-    return ([0, 0, 0]).map(function (x) { return Math.abs(Math.round(Math.random() * 255.0)); })
+    return ([0, 0, 0]).map(function (x) { return Math.abs(Math.round(Math.random() * 255.0)) })
 }
 
 function GetOrthogonalUnitVector(V) {
-    var W = [Math.random() * 10, Math.random() * -10, 0];
-    W[2] = (V[0] * W[0] + V[1] * W[1]) / (-1 * (V[2] == 0 ? 1 : V[2]));
-    var size = L2Norm(W);
-    return W.map(function (x) { return x / size; });
+    var W = [Math.random() * 10, Math.random() * -10, 0]
+    W[2] = (V[0] * W[0] + V[1] * W[1]) / (-1 * (V[2] == 0 ? 1 : V[2]))
+    var size = L2Norm(W)
+    return W.map(function (x) { return x / size; })
 }
 
 function L2Norm(V) {
-    return Math.sqrt(Math.pow(V[0], 2) + Math.pow(V[1], 2) + Math.pow(V[2], 2));
+    return Math.sqrt(Math.pow(V[0], 2) + Math.pow(V[1], 2) + Math.pow(V[2], 2))
 }
 
 function DotProduct(P, Q) {
-    return P[0] * Q[0] + P[1] * Q[1] + P[2] * Q[2];
+    return P[0] * Q[0] + P[1] * Q[1] + P[2] * Q[2]
 }
